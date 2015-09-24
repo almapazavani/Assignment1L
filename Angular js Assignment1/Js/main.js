fdf38fe51344b8c426cbd90d29886b3b113a17ad
almapazavani <avani.thakkar@gatewaytechnolabs.com>
2015-09-24 12:29:17 +0530

    Assignment1: Login module

---
+++ Angular js Assignment1/Js/main.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../bower_components/angular/angular.js" />
+
+var app = angular.module("logindemo", [])
+    .controller("LoginController", function ($scope) {
+        $scope.checkLogin = function () {
+            if ($scope.username === "nosql" && $scope.password === "html5") {
+                alert("Wellcome to hell");
+            }
+            else {
+                alert("Invalid User");
+            }
+        };
+    });
